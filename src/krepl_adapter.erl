bd339ccee2b61c27263dfd40c560c9d8010dcbd9
odo <odo@mac.com>
2014-03-22 21:21:34 +0100

    adding example callback module

--- src/krepl_adapter.erl
+++ src/krepl_adapter.erl
@@ -3,7 +3,7 @@
 -behaviour(gen_server).
 -export([start_link/4, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, status/1]).
 
--record(state, { consume_topic, consumer, calls, report }).
+-record(state, { consume_topic, consumer, calls, report, callback_module, callback_state }).
 
 %% Public API
 
@@ -40,27 +40,36 @@ handle_info({init, {ConsumerHost, ConsumerPort, {ConsumerTopicString, CallbackDa
     Offset         = krepl_offsets:offset(ConsumerTopic),
     ok             = kafka_consumer:set_offset(Consumer, Offset),
 
+    {ok, CallbackState}  = CallbackModule:init(ConsumerTopic, CallbackData),
+
     lager:info("Starting replication from ~p \t using ~p \t at offset ~p .", [ConsumerTopic, CallbackModule, Offset]),
     % enter the loop
     self() ! adapt,
 
-    State = #state{ consume_topic = ConsumerTopic, consumer = Consumer, calls = 1, report = Report },
+    State = #state{ consume_topic = ConsumerTopic, consumer = Consumer, calls = 1, report = Report,
+        callback_module = CallbackModule, callback_state = CallbackState },
     {noreply, State};
 
 handle_info(adapt, State =
-    #state{ consumer = Consumer, consume_topic = ConsumerTopic, calls = Calls }) ->
+    #state{ consumer = Consumer, consume_topic = ConsumerTopic, calls = Calls,
+        callback_module = CallbackModule, callback_state = CallbackState }) ->
 
+    CallbackStateNew =
     case kafka_consumer:fetch_with_offset(Consumer) of
         {ok, {_, []}} ->
-            erlang:send_after(500, self(), adapt);
+            erlang:send_after(500, self(), adapt),
+            CallbackState;
         {ok, {ConsumerOffset, Msgs}} ->
-            io:format("~p \t~p", [ConsumerTopic, hd(Msgs)]),
+
+            {ok, CallbackStateReply} = CallbackModule:handle(Msgs, ConsumerOffset, CallbackState),
+
             %{ok, _ProducerOffset} = kafka_producer:produce_ack(ProducerTopic, 0, Msgs, Producer),
             krepl_offsets:set_offset(ConsumerTopic, ConsumerOffset),
-            self() ! adapt
+            self() ! adapt,
+            CallbackStateReply
     end,
     maybe_report_lag(State),
-    {noreply, State#state{ calls = Calls + 1 }}.
+    {noreply, State#state{ calls = Calls + 1, callback_state = CallbackStateNew }}.
 
 terminate(_Reason, _State) ->
     ok.
---
+++ src/krepl_example_handler.erl
@@ -0,0 +1,11 @@
+-module(krepl_example_handler).
+
+-export([init/2, handle/3]).
+
+init(Topic, InitData) ->
+    io:format("init: ~p\n", [{Topic, InitData}]),
+    {ok, {Topic, InitData}}.
+
+handle(Msgs, Offset, State) ->
+    io:format("handle with msg: ~p offset: ~p state: ~p\n", [length(Msgs), Offset, State]),
+    {ok, State}.
