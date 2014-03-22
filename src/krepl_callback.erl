4557dbadce7c940ddbd05f23f4d1d22ae3a74cbc
odo <odo@mac.com>
2014-03-23 00:08:10 +0100

    fixing behaviour

--- src/krepl_callback.erl
+++ src/krepl_callback.erl
@@ -3,6 +3,7 @@
 -callback init(Topic :: list(), InitData :: term()) ->
     tuple('ok', State :: term()) | tuple('error', Reason :: term()).
 
--callback handle(Topic :: list(), Offset :: integer(), State :: term()) ->
+-callback handle(Massages :: list(binary()), Offset :: integer(), State :: term()) ->
     tuple('ok', State :: term()) | tuple('error', Reason :: term()).
+
 
